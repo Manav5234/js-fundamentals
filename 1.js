8e7881a357d0cd70a75e20a7ac12fecd59e63686
Manav <manavmk5234@gmail.com>
2026-03-30 20:56:10 +0530

    Day 1: variables, data types, console.log

--- 1.js
+++ 1.js
@@ -1,3 +1,23 @@
-console.log("Hello, I am Manav");
-console.log(2025);
-console.log(true);
+// My profile in JavaScript
+
+const myName = "Manav";
+const myCollege = "IIIT Sonepat";
+const myYear = 1;
+const myAge = 18;
+const isCodingEveryDay = true;
+
+console.log(`My name is ${myName}.`);
+console.log(`I study at ${myCollege}.`);
+console.log(`I am in year ${myYear}, age ${myAge}.`);
+console.log(`Am I coding every day? ${isCodingEveryDay}`);
+
+const hoursPerDay = 2.5;
+const daysInMonth = 30;
+const totalHours = hoursPerDay * daysInMonth;
+console.log(`Hours I will code this month: ${totalHours}`);
+
+
+let x;
+console.log(`x before value: ${x}`);
+x = 100;
+console.log(`x after value: ${x}`);
